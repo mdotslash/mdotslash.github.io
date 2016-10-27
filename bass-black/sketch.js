c3626cb326fb790cc0f5e6401f902683bf5716b0
Mayowa Tomori <hi@mayowatomori.com>
2016-10-27 08:12:24 -0700

    add reverb, instructions, update polaroid frame design

--- bass-black/sketch.js
+++ bass-black/sketch.js
@@ -6,6 +6,16 @@
 // bass black plays a sound. lower the tone, the darker the pantone
 // load image array? (ACTUALLY FUCK IT. LETS USE A GIF)
 
+  //variables
+  //quote/text
+  
+// var s = "'I believe the day will come when all God’s children from bass black to treble white,"
+// var s2 = "will be significant on the Constitution’s keyboard.'"
+// var q = "Martin Luther King Jr";
+var inst = "Play me with 'a', 's', 'd', 'f', 'g'"
+  
+  
+  
 // preload sounds
 
 function preload() {
@@ -15,10 +25,104 @@ function preload() {
   mySound3 = loadSound('snd/4.wav')
   mySound4 = loadSound('snd/5.wav')
   mySound5 = loadSound('snd/6.wav')
+}
+
+function setup() {
+  createCanvas(800, 800);
+  
+
+  //images etc
+  imgArr = loadGif("img/bass.gif"); // load a gif
+  imgArr.pause(); // pause it
+  imgFrame = loadImage("img/frame.png") // shitty png of a polaroid
+  
+  //reverb trail setup
+  reverb = new p5.Reverb();
+  //mySound.connect(); // COnnect output to reverb
+  
+  // connect soundFile to reverb, process w/
+  // 3 second reverbTime, decayRate of 2%
+  reverb.process(mySound5, 1, 2);
+  reverb.process(mySound4, 3, 2);
+  reverb.process(mySound3, 3, 2);
+  reverb.process(mySound2, 3, 2);
+  reverb.process(mySound1, 3, 2);
+  reverb.process(mySound, 5, 2);
   
 }
 
-// Preload functions that call sound
+// show image array
+function draw() {
+  background(255);
+  
+   // display gif
+  if (imgArr.loaded()) {
+    image(imgArr, 100, 175); // gif
+    image(imgFrame, 0, 100);  // gif frame
+}
+
+// //text / quote display
+  
+  textSize(30);
+  text(inst, 150, 75);
+//   textSize(15);
+//   text(s, 85,675);
+//   text(s2, 85, 700)
+//   textSize(25);
+//   text(q, 85, 730);
+  
+ 
+}
+
+// test frame scrub functionality
+
+// function mouseMoved() {
+//   if (imgArr.loaded()) {
+//     var frame = int( map(mouseX, 0, width, 0, imgArr.totalFrames() ) );
+//     imgArr.frame(frame);
+//     console.log(frame); // we have 6 possible values to send it, from 0 - 5
+//   }
+// }
+
+// Now let's map each frame to a button from h to a 
+// (where h refers to the lightest Pantone value and a the lowest)
+// to a gif frame.
+
+function keyTyped() {
+  if (key === 'h') {
+    imgArr.frame(0);
+    //Hornsy5();
+    mySound5.play();
+    console.log(key); // we have 6 possible values to send it, from 0 - 5
+  } else if (key === 'g') {
+    imgArr.frame(1);
+    mySound4.play();
+    //Hornsy4();
+    console.log(key); // we have 6 possible values to send it, from 0 - 5;
+  } else if (key === 'f') {
+    imgArr.frame(2);
+    mySound3.play();
+    //Hornsy3();
+    console.log(key); // we have 6 possible values to send it, from 0 - 5;
+  } else if (key === 'd') {
+    imgArr.frame(3);
+    mySound2.play();
+    //Hornsy2();
+    console.log(key); // we have 6 possible values to send it, from 0 - 5;
+  } else if (key === 's') {
+    imgArr.frame(4);
+    mySound1.play();
+    //Hornsy1();
+    console.log(key); // we have 6 possible values to send it, from 0 - 5;
+  } else if (key === 'a') {
+    imgArr.frame(5);
+    mySound.play();
+    console.log(key);
+    //Hornsy();
+  }
+}
+
+// Functions that call sound
 
 function Hornsy() {
     mySound.setVolume(0.1);
@@ -49,93 +153,3 @@ function Hornsy5() {
     mySound5.setVolume(0.1);
     mySound5.play();
 }
-
-//}
-
-function setup() {
-  imgArr = loadGif("img/bass.gif"); // load a gif
-  imgArr.pause(); // pause it
-  imgFrame = loadImage("img/frame3.png") // shitty png of a polaroid
-  //oscH
-  createCanvas(800, 800);
-}
-
-// show image array
-function draw() {
-  background(255);
-  if (imgArr.loaded()) {
-    image(imgArr, 100, 75); // gif
-    image(imgFrame, 0, 0);  // gif frame
-}
-}
-
-// test frame scrub functionality
-
-// function mouseMoved() {
-//   if (imgArr.loaded()) {
-//     var frame = int( map(mouseX, 0, width, 0, imgArr.totalFrames() ) );
-//     imgArr.frame(frame);
-//     console.log(frame); // we have 6 possible values to send it, from 0 - 5
-//   }
-// }
-
-// Now let's map each frame to a button from h to a 
-// (where h refers to the lightest Pantone value and a the lowest)
-// to a gif frame.
-
-function keyTyped() {
-  if (key === 'h') {
-    imgArr.frame(0);
-    Hornsy5();
-    console.log(key); // we have 6 possible values to send it, from 0 - 5
-  } else if (key === 'g') {
-    imgArr.frame(1);
-    Hornsy4();
-    console.log(key); // we have 6 possible values to send it, from 0 - 5;
-  } else if (key === 'f') {
-    imgArr.frame(2);
-    Hornsy3();
-    console.log(key); // we have 6 possible values to send it, from 0 - 5;
-  } else if (key === 'd') {
-    imgArr.frame(3);
-    Hornsy2();
-    console.log(key); // we have 6 possible values to send it, from 0 - 5;
-  } else if (key === 's') {
-    imgArr.frame(4);
-    Hornsy1();
-    console.log(key); // we have 6 possible values to send it, from 0 - 5;
-  } else if (key === 'a') {
-    imgArr.frame(5);
-    Hornsy();
-  }
-}
-
-// Now let's map each frame to a key from h to a that plays a sound
-// (where h refers to the highest note melodically value and a the lowest)
-
-
-// function soundTyped() {
-//     if (key === 'h') {  // play doorbell if you hear smth
-//       mySound.setVolume(0.1);
-//       mySound.play();
-//     console.log(key); // we have 6 possible values to send it, from 0 - 5
-//   } else if (key === 'g') {
-//     imgArr.frame(1);
-//     console.log(nope); // we have 6 possible values to send it, from 0 - 5;
-  // } else if (key === 'f') {
-  //   imgArr.frame(2);
-  //   console.log(key); // we have 6 possible values to send it, from 0 - 5;
-  // } else if (key === 'd') {
-  //   imgArr.frame(3);
-  //   console.log(key); // we have 6 possible values to send it, from 0 - 5;
-  // } else if (key === 's') {
-  //   imgArr.frame(4);
-  //   console.log(key); // we have 6 possible values to send it, from 0 - 5;
-  // } else if (key === 's') {
-  //   imgArr.frame(4);
-  //   console.log(key); // we have 6 possible values to send it, from 0 - 5;
-  // } else if (key === 'a') {
-  //   imgArr.frame(5);
-  // }
-// }
-// }
